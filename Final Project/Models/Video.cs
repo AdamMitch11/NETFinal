6ac742816eec24238bdbbe6175492df9b21c2a12
bhanson5 <bhanson5@my.wctc.edu>
2015-04-20 14:05:04 -0500

    first commit

---
+++ Final Project/Models/Video.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Final_Project.Models
+{
+    public class Video
+    {
+        public int ID { get; set; }
+        public 
+    }
+}
