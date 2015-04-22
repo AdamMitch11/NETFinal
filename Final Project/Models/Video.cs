57954901986e749b9688c129667f7ccbd67a1bad
bhanson5 <bhanson5@my.wctc.edu>
2015-04-22 13:33:01 -0500

    asd

--- Final Project/Models/Video.cs
+++ Final Project/Models/Video.cs
@@ -7,7 +7,15 @@ namespace Final_Project.Models
 {
     public class Video
     {
-        public int ID { get; set; }
-        public 
+        public int VideoId { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public int Views { get; set; }
+        public int AuthorId { get; set; }
+        public string VideoSrc { get; set; }
+        public string Description { get; set; }
+        public Author author { get; set; }
+        public Catagory catagory { get; set; }
+
     }
 }
